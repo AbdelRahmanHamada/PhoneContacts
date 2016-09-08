f1445dd6e285de20c83a087b0192c874dccb8990
AbdelRahman Hamada <abed_hamada@hotmail.com>
2016-09-09 00:58:16 +0300

    Implement Contact class

---
+++ src/phonecontacts/Contact.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phonecontacts;
+
+/**
+ *
+ * @author abed
+ */
+public class Contact {
+    private String name;
+    private String[] phones;
+
+    public Contact() {
+    }
+
+    public Contact(String name, String[] phones) {
+        this.name = name;
+        this.phones = phones;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getPhones() {
+        return phones;
+    }
+
+    public void setPhones(String[] phones) {
+        this.phones = phones;
+    }
+    
+    
+    
+}
